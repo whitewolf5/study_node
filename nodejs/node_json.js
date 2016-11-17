b47583f81a02c7adf655fb368ce5ac4ebd313524
곽노환 <whitewolf@gwagnohwan-ui-MacBook-Pro.local>
2016-11-17 18:44:20 +0900

    JSON length로직추가

--- nodejs/node_json.js
+++ nodejs/node_json.js
@@ -8,11 +8,13 @@ function a(){
 
     // JSON 문자열을 JavaScript 오브젝트로 변환한다. 주로 원격 서버로부터의 AJAX 응답시 반환 데이터를 변환하기 위해 사용한다.
     var object = JSON.parse(json);
+    var length = Object.keys(object).length; //json사이즈
 
     // JavaScript 오브젝트를 JSON 문자열로 변환한다. 주로 원격 서버에 AJAX 요청시 파라메터를 전달하기 위해 사용한다.
     var json = JSON.stringify(object);
     var json2 = JSON.stringify(object, null, 4); //트리형으로 보여준다.
 
+    console.log("json length=", length);
     console.log(json);
     console.log(json2);
 }
